4e84d7867a9c3d7604749e67450305006abdd1f2
madhavyashIGC <64628716+madhavyashIGC@users.noreply.github.com>
2021-07-25 11:07:36 +0530

    Add files via upload

---
+++ yo.py
@@ -0,0 +1,21 @@
+# Calculator with python (if  command used) .
+a = input('enter first no. ')
+a = int(a)
+
+b = input('enter second no. ')
+b  = int(b)
+
+c = input('enter Operation  ')
+
+# Now
+if c == '*' :
+    print(a*b)
+
+if c == '-':
+    print(a-b)
+
+if c == '/':
+    print(a/b)
+
+if a == '+':
+    print(a+b)
